4e094d05cd8ebd8aadeb929383419b77e67581f7
Tyler Gray <tyler.gray@gmail.com>
2020-06-11 09:48:26 -0500

    Initial commit

---
+++ api/src/main/java/com/astroviking/petpals/PetPalsApplication.java
@@ -0,0 +1,12 @@
+package com.astroviking.petpals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPalsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PetPalsApplication.class, args);
+  }
+}
---
+++ api/src/test/java/com/astroviking/petpals/PetPalsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.astroviking.petpals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetPalsApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
