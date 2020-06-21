52419dd64e74e7dfa33926faf4fd962194401ec4
Tyler Gray <tyler.gray@gmail.com>
2020-06-21 11:05:22 -0500

    Created initial components.

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import { BannerComponent } from './banner/banner.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { InfoComponent } from './info/info.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    NavBarComponent,
+    InfoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
