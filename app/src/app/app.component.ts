ec9858b47e4312cf6993047cefd33ec6a16b9f88
Hannah Gray <hannah.rose.gray@gmail.com>
2020-06-21 14:32:18 -0500

    Added flex layout, css, and side navbar

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PetPals.io';
+  title = 'PawPals.io';
   openSideNav = false;
 }
