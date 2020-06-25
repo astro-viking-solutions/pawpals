2fc729838a6746b5809e5aea12c4d7b7112fd3fe
Tyler Gray <tyler.gray@gmail.com>
2020-06-24 19:52:02 -0500

    API Authentication

--- api/src/main/java/com/astroviking/pawpals/controllers/UserController.java
+++ api/src/main/java/com/astroviking/pawpals/controllers/UserController.java
@@ -1,6 +1,10 @@
 package com.astroviking.pawpals.controllers;
 
 import com.astroviking.pawpals.domain.User;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-  @GetMapping("/info")
-  public User getCurrentUser() {
+  @GetMapping
+  public User getCurrentUser(@AuthenticationPrincipal Jwt jwt) {
     User user = new User();
     user.setFirstName("First");
     user.setLastName("Last");
